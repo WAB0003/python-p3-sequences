6cfa13e67d75cd7206853b26d44bd257d6a4b9c3
Bill Brown <bill.a.brown90@gmail.com>
2023-04-03 18:34:43 -0700

    sequencing

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    
+    if length == 0:
+        print([])
+    elif length == 1:
+        print([0])
+    else: 
+        s = [0,1]
+        i = 1
+        while i < length-1:
+            next_number = s[i-1] + s[i]
+            s.append(next_number)
+            i += 1
+        print(s)
